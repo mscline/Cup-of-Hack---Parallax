5220f66b284787b78bd0dee3c37392f654ac5db6
mscline <mychiloscline@gmail.com>
2015-02-12 14:14:14 -0800

    Initial Commit

---
+++ prlx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  prlx
+//
+//  Created by xcode on 2/12/15.
+//  Copyright (c) 2015 MSCline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ prlx/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  prlx
+//
+//  Created by xcode on 2/12/15.
+//  Copyright (c) 2015 MSCline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
